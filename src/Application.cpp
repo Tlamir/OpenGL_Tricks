99e5245b54da3c89595962b077bdd5bfac639d09
Yigithan Guven <demirejderha@gmail.com>
2025-02-15 19:02:48 +0300

    shader parsing

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,11 @@
 #include<GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
 // Every cpp code done in CPU
 // 
 // OpenGL operates like state machine
@@ -16,6 +21,48 @@
 // 
 // Vertex shader called by each vertecies. (3 in Triangle)
 // Fragment shader called by each pixels and colors. (too many times)
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& fillepath)
+{
+	std::ifstream stream(fillepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				// Set mode to vertex
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				// Set mode to fragment
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(),ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -107,28 +154,11 @@ int main(void)
 
 	//glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
+	ShaderProgramSource source = ParseShader("res/shaders/Shader.shader");
+	std::cout << source.VertexSource << std::endl;
+	std::cout << source.FragmentSource << std::endl;
 
-	std::string fragmentShader =
-		"#version 330 core \n"
-		"\n"
-		"layout(location = 0) out vec4 color;"
-		"\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		" color = vec4(0.0,1.0,0.0,1.0);\n"  // r g b a
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
